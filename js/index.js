dada4e5f5a066cef1fe5b756f0a49c7c14b3b640
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-05-20 22:25:24 +0200

    adds start files

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const refs = {
+  buttonStart: document.querySelector('button[data-action="start"]'),
+  buttonStop: document.querySelector('button[data-action="stop"]'),
+};
