160cafb50e6fbbc6d4867a5a82fc9b481269c9ec
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-05-21 13:52:24 +0200

    adds js code

--- js/index.js
+++ js/index.js
@@ -6,8 +6,39 @@ const colors = [
   "#009688",
   "#795548",
 ];
+// console.log(colors.length);
 
 const refs = {
   buttonStart: document.querySelector('button[data-action="start"]'),
   buttonStop: document.querySelector('button[data-action="stop"]'),
+  bodyContainer: document.querySelector("body"),
 };
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let timerId = null;
+
+const onButtonStartClick = (evt) => {
+  if (!evt.target.dataset.action === "start") {
+    return;
+  }
+  refs.buttonStart.removeEventListener("click", onButtonStartClick);
+  timerId = setInterval(changeColors, 1000);
+  console.log("button start was clicked");
+};
+
+const onButtonStopClick = () => {
+  refs.buttonStart.addEventListener("click", onButtonStartClick);
+  clearInterval(timerId);
+};
+
+const changeColors = () => {
+  let colorIndex = randomIntegerFromInterval(0, colors.length);
+  console.log(colorIndex);
+  refs.bodyContainer.style.backgroundColor = colors[colorIndex];
+};
+
+refs.buttonStart.addEventListener("click", onButtonStartClick);
+refs.buttonStop.addEventListener("click", onButtonStopClick);
